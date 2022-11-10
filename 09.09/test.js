77d67be1e94068f6ca7442f724dde0e7cde9053d
Egle Vestrik <egleves@tlu.ee>
2022-11-10 23:43:30 +0200

    first commit

---
+++ 09.09/test.js
@@ -0,0 +1,35 @@
+console.log('hi')
+
+function sayHI(){
+
+    
+}
+
+//kirjutage 3 funktsiooni tüüpi, a ja b ja returnib summa
+
+const shortArrowFunction=()=>'hello from short arrow'
+
+//sum(3,4) =>
+
+function first (){
+    return a*B
+}
+
+const second=()=>a*b
+
+
+
+
+//kirjutada see ümber funktsiooni kujul 
+
+//const sumA = a => b => a+b
+
+
+function sumA(a){
+    return function sumB(b){
+        return a+b
+    }
+}
+
+
+console.log(sumA(3)(4))
